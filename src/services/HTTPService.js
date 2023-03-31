6a3f7ca2801b1f1c95db27aefa0a0884aca4f7fa
Yanzen Project <yanwarsolah@gmail.com>
2023-03-31 11:54:39 +0700

    add loading indicator

--- src/services/HTTPService.js
+++ src/services/HTTPService.js
@@ -4,7 +4,14 @@ const HTTPService = axios.create({
   timeout: 1000,
 });
 
+HTTPService.interceptors.request.use((response) => {
+  // console.log("Response:", JSON.stringify(response, null, 2));
+  document.body.classList.add("loading-indicator");
+  return response;
+});
+
 HTTPService.interceptors.response.use((response) => {
+  document.body.classList.remove("loading-indicator");
   // console.log("Response:", JSON.stringify(response, null, 2));
   if (response.status === 401) {
     window.location.href = "/";
